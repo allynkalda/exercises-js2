b4e1d9622829d93e3429b19030fb76cae22ac680
allyn <allyn.alda@gmail.com>
2020-09-14 21:38:57 +0200

    finished homework 1

--- week-3/Homework/1-alarmclock/alarmclock.js
+++ week-3/Homework/1-alarmclock/alarmclock.js
@@ -1,4 +1,23 @@
-function setAlarm() {}
+function setAlarm() {
+  let seconds = document.getElementById("alarmSet").value;
+  document.getElementById("alarmSet").value = '';
+  const formattedNumber = (myNumber) => ("0" + myNumber).slice(-2);
+  const setSeconds = () => {
+    seconds--;
+    document.getElementById("timeRemaining").innerHTML = `Time Remaining: 00:${formattedNumber(seconds)}`;
+    if (seconds === 0) {
+      clearInterval(idInterval);
+    }
+  };
+  const idInterval = setInterval(setSeconds, 1000);
+  
+  const setAlarmSeconds = () => {
+    playAlarm();
+    document.body.style.color = "white";
+    document.body.style.backgroundColor = "red";
+  }
+  setTimeout(setAlarmSeconds, seconds * 1000);
+}
 
 // DO NOT EDIT BELOW HERE
 
